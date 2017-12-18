b29fe1e47b020c997ec5f8bcca71dd12770a07e3
Mohamed Ibrahim <mohamed.ibrahim1@uoit.net>
2017-12-17 20:48:13 -0500

    convert to MVC design pattern

---
+++ Todoey/Data Model/Item.swift
@@ -0,0 +1,15 @@
+//
+//  Item.swift
+//  Todoey
+//
+//  Created by Mohamed Ibrahim on 2017-12-17.
+//  Copyright © 2017 Mohamed Ibrahim. All rights reserved.
+//
+
+import Foundation
+
+
+class Item {
+    var title: String = ""
+    var done: Bool = false
+}
